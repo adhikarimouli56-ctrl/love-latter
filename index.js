7b339c88eb4e5e5f465152b49dd5ff62949ae70f
adhikarimouli56-ctrl <adhikarimouli56@gmail.com>
2026-03-31 14:50:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function showMsg(){
+  let msg = document.getElementById("msg");
+
+  if(msg.style.display === "none"){
+    msg.style.display = "block";
+  } else {
+    msg.style.display = "none";
+  }
+}
