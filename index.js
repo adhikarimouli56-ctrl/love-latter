328ae94a2e108d4fb906f68f7d693ca9fc921ee4
adhikarimouli56-ctrl <adhikarimouli56@gmail.com>
2026-03-31 20:11:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-
-function showMsg(){
-  let msg = document.getElementById("msg");
-
-  if(msg.style.display === "none"){
-    msg.style.display = "block";
-  } else {
-    msg.style.display = "none";
-  }
+function showLove() {
+    document.getElementById("hiddenMsg").style.display = "block";
 }
